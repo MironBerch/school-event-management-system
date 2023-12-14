3e02c0bd1e6d82e4a5a842a71554f24ed710e1c9
MironBerch <mironberchetov@gmail.com>
2023-12-15 01:54:55 +0300

    Add postgres service
    Add postgres service to docker-compose. Update project
    requirements files. Edit project database settings.

--- school_event_management_system/config/settings.py
+++ school_event_management_system/config/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': environ.get('DB_NAME'),
+        'USER': environ.get('DB_USER'),
+        'PASSWORD': environ.get('DB_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     },
 }
 
