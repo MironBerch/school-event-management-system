97cdff0807fc7f20b13efa7b36c2137c4077eeab
MironBerch <mironberchetov@gmail.com>
2024-01-04 06:10:42 +0300

    Update mailings forms, views and html files

--- school_event_management_system/mailings/forms.py
+++ school_event_management_system/mailings/forms.py
@@ -2,6 +2,17 @@ from ckeditor.fields import RichTextFormField
 
 from django import forms
 
+from events.models import Event
+
 
 class MailingForm(forms.Form):
-    content = RichTextFormField()
+    content = RichTextFormField(
+        label='содержание письма',
+    )
+
+
+class RecipientsForm(forms.Form):
+    event = forms.ModelChoiceField(
+        queryset=Event.objects.all(),
+        label='мероприятие',
+    )
--- school_event_management_system/mailings/views.py
+++ school_event_management_system/mailings/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from django.views.generic import View
 from django.views.generic.base import TemplateResponseMixin
 
-from mailings.forms import MailingForm
+from mailings.forms import MailingForm, RecipientsForm
 
 
 class MailingCreateView(
@@ -13,6 +13,7 @@ class MailingCreateView(
 ):
     template_name = 'mailings/mailing_create.html'
     mailing_form: MailingForm = None
+    recipients_form: RecipientsForm = None
 
     def dispatch(self, request: HttpRequest, *args, **kwargs):
         if not request.user.is_authenticated:
@@ -22,11 +23,25 @@ class MailingCreateView(
         self.mailing_form = MailingForm(
             data=request.POST or None,
         )
+        self.recipients_form = RecipientsForm(
+            data=request.POST or None,
+        )
         return super(MailingCreateView, self).dispatch(request, *args, **kwargs)
 
     def get(self, request: HttpRequest):
         return self.render_to_response(
             context={
                 'mailing_form': self.mailing_form,
+                'recipients_form': self.recipients_form,
+            }
+        )
+
+    def post(self, request: HttpRequest):
+        if self.recipients_form.is_valid() and self.mailing_form.is_valid():
+            pass
+        return self.render_to_response(
+            context={
+                'mailing_form': self.mailing_form,
+                'recipients_form': self.recipients_form,
             }
         )
